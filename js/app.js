2e166e747935a717d2ccf0cccdfa7499a21b1c37
raffa <raffa.rayen@gmail.com>
2020-03-24 18:17:40 +0100

    updated enemy behaviour

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    // player.handleInput(allowedKeys[e.keyCode]);
 });
 
 
